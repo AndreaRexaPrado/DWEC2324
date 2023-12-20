20a481192b16caf0fc3172bf173794a62d641bb0
andrearexa <andrea_rexa@outlook.es>
2023-12-20 10:39:03 +0100

    Buscaminas

--- Ejercicios/Buscaminas/iniciarBuscaminas.js
+++ Ejercicios/Buscaminas/iniciarBuscaminas.js
@@ -1,16 +1,29 @@
+let numMinas=0;
+let matriz=[];
+let tablero = document.getElementById("tablero");
+
 function dibujarTableroHTML(ancho,alto){
-    let tablero="<table>";
- 
-    for(let i=0;i< alto;i++) {
-        tablero+="<tr>"
-        for(let j=0;j< ancho;j++) {
-            tablero+="<td id='"+i+j+"'><button id='"+i+j+"'></button</td>"
-        }
-        tablero+="</tr>"
+    if (tablero && tablero.childNodes.length > 0) {
+        tablero.removeChild(tablero.childNodes[0]);
     }
-    tablero+="</table>";
-    return tablero;
+
+    let tabla = document.createElement("table");
+    for(let i=0;i< alto;i++) {
+        let row = tabla.insertRow(i)
+        matriz[i]= new Array(ancho);
+
+        for(let j=0;j< ancho;j++) {
+            let cell = row.insertCell(j);
+            cell.innerHTML="<button id='"+i+j+"'>X</button>"
+            matriz[i][j]=0;
+        }
+        
+    }
+
+    console.log(matriz);
+    tablero.appendChild(tabla);
 }
+
 var select = document.getElementById("selectMinas");
  
 select.addEventListener("change", function(){
@@ -18,31 +31,41 @@ select.addEventListener("change", function(){
 
   switch(selectedOption.value){
     case "op1":
-        document.getElementById("tablero").innerHTML= dibujarTableroHTML(9,9);
+        dibujarTableroHTML(9,9);
+        numMinas=10;
         break;
     case "op2":
-        document.getElementById("tablero").innerHTML= dibujarTableroHTML(9,9);
+        dibujarTableroHTML(9,9);
+        numMinas=35;
         break;
     case "op3":
-        document.getElementById("tablero").innerHTML= dibujarTableroHTML(16,16);
+        dibujarTableroHTML(16,16);
+        numMinas=40;
         break;
     case "op4":
-        document.getElementById("tablero").innerHTML= dibujarTableroHTML(16,16);
+        dibujarTableroHTML(16,16);
+        numMinas=99;
         break;
     case "op5":
-        document.getElementById("tablero").innerHTML= dibujarTableroHTML(30,16);
+        dibujarTableroHTML(30,16);
+        numMinas=99;
         break;
     case "op6":
-        document.getElementById("tablero").innerHTML= dibujarTableroHTML(30,16);
+        dibujarTableroHTML(30,16);
+        numMinas=170;
         break;
     case "person":
         let ancho = prompt("Dame el ancho: ");
         let alto = prompt("Dame el alto: ");
-        document.getElementById("tablero").innerHTML= dibujarTableroHTML(ancho,alto);
+        dibujarTableroHTML(ancho,alto);
         break;
   }
 });
-document.getElementById("tablero").innerHTML= dibujarTableroHTML(9,9);
 
-//console.log(dibujarTableroHTML(9,9));
+dibujarTableroHTML(9,9);
+numMinas=10;
 
+function colocarBombasTableroJS(){
+
+    
+}
