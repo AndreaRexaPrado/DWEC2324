600d312bf18c797aed05f85356ae9c43224afbc0
andrearexa <andrea_rexa@outlook.es>
2024-02-08 13:51:18 +0100

    1

--- Practica202/202ARP.js
+++ Practica202/202ARP.js
@@ -2,6 +2,7 @@
 window.onload = inicio;
 
 function inicio(){
+    borrarCampos();
     let cont = 0;
     document.getElementById("caract").innerHTML=cont;
     //Boton ir a google
@@ -32,17 +33,7 @@ function inicio(){
             dia.checked=true;
         }
     });
-
-
-    //Contador caracteres mensajes
-    /*document.getElementById("mensaje").addEventListener("input",(e)=>{
-        console.log(e.key)
-        if(e.key !== 'Backspace'){
-            cont++;
-            document.getElementById("caract").innerHTML=cont;
-        }
-    });*/
-
+    //Contador caracteres
     document.getElementById("mensaje").addEventListener("keydown",(e)=>{
         if (cont === 0 && e.inputType === 'deleteContentBackward') {
             return; 
@@ -54,6 +45,86 @@ function inicio(){
             cont = e.target.value.length ;
         }
     
-        document.getElementById("caract").innerHTML = cont;
+        
+        if(cont > 500){
+            document.getElementById("caract").innerHTML = 500-cont;
+            document.getElementById("caract").className = "contRojo";
+        }else{
+            document.getElementById("caract").innerHTML = cont;
+        }
     });
+
+    document.getElementById("enviar").addEventListener('click',validar);
 }
+
+    function validar(e){
+    validarDNI();
+
+        borrarError();
+        if(camposVacios()&&confirm("Estas seguro de tus datos")){
+            alert("Tus datos han sido enviados");
+            borrarCampos();
+            return true;
+        }else{
+            e.preventDefault();
+            return false;
+        }
+    }
+
+    function calcularLetraDNI(numeroDNI) {
+        const letrasDNI = "TRWAGMYFPDXBNJZSQVHLCKE";
+        const numero = numeroDNI % 23;
+        const letra = letrasDNI.charAt(numero);
+        return letra;
+    }
+
+    //Validaciones
+
+    function camposVacios(){
+        let form = document.forms[0];
+
+        for(let ele of form.elements){
+            
+            if(ele.type ==="text"||ele.type ==="date"){
+                if(ele.value ===""){
+                    error(ele,"Este campo no debe estar vacio");
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    function validarDNI(){
+        let inDNI= document.getElementById('nif');
+        let numdni= inDNI.substring(0,8)
+        console.log(numdni)
+    }
+    function error(elemento, mssg){
+        
+        document.getElementById("mensajeError").innerHTML = mssg;
+        elemento.className = "error";
+        elemento.focus();
+    }
+
+    function borrarError(){
+        document.getElementById("mensajeError").innerHTML="";
+        var form = document.forms[0];
+        for(var i=0;i<form.elements.length;i++){
+            form.elements[i].className="";
+        }
+    }
+
+    function borrarCampos(){
+
+        var form = document.forms[0];
+        for(var i=0;i<form.elements.length;i++){
+            let form = document.forms[0];
+
+            for(let ele of form.elements){
+                
+                if(ele.type ==="text"||ele.type ==="date"){
+                    ele.value =""
+                }
+            }
+        }
+    }
